24a3370e49b5494959f67d7c3751f1c96ed4df09
Carlos-vargs <cvargaslopez769@gmail.com>
2022-11-14 16:20:30 -0600

    ci: add forms request

---
+++ app/Http/Requests/StoreInvoiceRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreInvoiceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'total' => 'required',
+            'iva' => 'required',
+            'descuento' => 'required',
+            'modo_pago' => 'required',
+            'subtotal' => 'required',
+            'cantidad' => 'required',
+            'precio' => 'required',
+            'waiter_id' => 'required',
+            'products' => 'required|array',
+        ];
+    }
+}
---
+++ app/Http/Requests/StoreWaiterRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreWaiterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'cedula_empleado' => 'required|string',
+            'primer_nombre' => 'required|string',
+            'segundo_nombre' => 'nullable|string',
+            'primer_apellido' => 'required|string',
+            'segundo_apellido' => 'nullable|string',
+            'numero_telefono' => 'required|string',
+        ];
+    }
+}
